76851152252f60dbca103f6fb752af7d2d6efe6c
Sergii <77669556+sergiiiavt@users.noreply.github.com>
2026-08-29 13:11:03 +0300

    ci: configure Allure 3 report

---
+++ allurerc.mjs
@@ -0,0 +1,12 @@
+export default {
+  name: "Desktop Automation Lab",
+  output: "./allure-report",
+  plugins: {
+    awesome: {
+      options: {
+        singleFile: true,
+        reportLanguage: "en",
+      },
+    },
+  },
+};
